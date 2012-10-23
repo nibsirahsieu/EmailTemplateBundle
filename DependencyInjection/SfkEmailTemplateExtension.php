125024cb9aa9b7ddfc90d6160811ba5805ad94ad
Oleg K <getmequick@gmail.com>
2012-10-23 18:52:00 +0500

    allow default loader to be set (default_loader: foobar)

--- DependencyInjection/SfkEmailTemplateExtension.php
+++ DependencyInjection/SfkEmailTemplateExtension.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * SfkEmailTemplateExtension
@@ -21,8 +22,10 @@ class SfkEmailTemplateExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-        
+
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+        
+        $container->setAlias('sfk_email_template.loader', $config['default_loader']);
     }
 }
