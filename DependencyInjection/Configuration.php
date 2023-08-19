060a92090d5365deffdf51a46a553f027ba89ee0
Yunan NSNT <nibsirahsieu@gmail.com>
2023-08-19 15:43:28 +0700

    BC layer for symfony/config 4.1 and older

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('sfk_email_template');
+        $treeBuilder = new TreeBuilder('sfk_email_template');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root("sfk_email_template");
+        }
 
         $rootNode
             ->children()
